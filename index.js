80a1ad71aa8782c5d4050b3178b38e7951a96661
GauravKumar260100 <gaurav.kumar.mcs21@cs.du.ac.in>
2023-11-03 13:16:25 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Creating a funny name
+// let generateName = require("sillyname");
+// import generateName from "sillyname";
+
+// let myName = generateName();
+// console.log("My name is " + myName);
+
+import superheroes from "superheroes";
+let myName = superheroes.random();
+
+console.log("I am " + myName);
